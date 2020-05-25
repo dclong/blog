3c37bb2d55dade3a64eb2c8b59950dc90e190010
Ben Chuanlong Du <legendu.net@outlook.com>
2020-05-24 22:43:18 -0700

    ...

--- misc/pconf.py
+++ misc/pconf.py
@@ -5,16 +5,16 @@ from pathlib import Path
 from pelican_jupyter import markup as nb_markup
 #-------------------------------------------------------------------------
 # !!! http:// is necessary
-HOME_URL = "http://www.legendu.net"
-SITEURL = os.path.join(HOME_URL, "misc") 
+HOME_URL = Path("http://www.legendu.net")
+SITEURL = HOME_URL / "misc"
 #-------------------------------------------------------------------------
 SITESUBTITLE = "It is never too late to learn."
-AUTHOR = 'Ben Chuanlong Du'
+AUTHOR = "Ben Chuanlong Du"
 SITENAME = "Ben Chuanlong Du's Blog"
 # Times and dates
-DEFAULT_DATE_FORMAT = '%b %d, %Y'
-TIMEZONE = 'US/Pacific'
-DEFAULT_LANG = u'en'
+DEFAULT_DATE_FORMAT = "%b %d, %Y"
+TIMEZONE = "US/Pacific"
+DEFAULT_LANG = u"en"
 #-------------------------------------------------------------------------
 PATH = Path(__file__).resolve().parent
 BLOG_DIR = PATH.parent
@@ -25,25 +25,25 @@ PAGE_PATHS = ["pages"]
 DISPLAY_PAGES_ON_MENU = False
 
 # Set the article URL
-ARTICLE_URL = 'blog/{slug}/'
-ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
+ARTICLE_URL = "blog/{slug}/"
+ARTICLE_SAVE_AS = "blog/{slug}/index.html"
 
 MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.codehilite': {'css_class': 'highlight'},
-        'markdown.extensions.extra': {},
-        'markdown.extensions.meta': {},
-        'markdown.extensions.toc': {},
+    "extension_configs": {
+        "markdown.extensions.codehilite": {"css_class": "highlight"},
+        "markdown.extensions.extra": {},
+        "markdown.extensions.meta": {},
+        "markdown.extensions.toc": {},
     },
-    'output_format': 'html5',
+    "output_format": "html5",
 }
 
 # Title menu options
 MENUITEMS = [
-    ('Home', HOME_URL),
-    ('Blog', SITEURL), 
-    ("Archives", os.path.join(SITEURL, 'archives.html')), 
-    ('Links', os.path.join(SITEURL, 'pages/links.html')), 
+    ("Home", HOME_URL),
+    ("Blog", SITEURL), 
+    ("Archives", SITEURL / "archives.html"),
+    ("Links", SITEURL / "pages/links.html"),
 ]
 NEWEST_FIRST_ARCHIVES = True
 
@@ -52,7 +52,7 @@ TAG_CLOUD_STEPS = 10
 TAG_CLOUD_MAX_ITEMS = 100
 
 #Github include settings
-GITHUB_USER = 'dclong'
+GITHUB_USER = "dclong"
 GITHUB_REPO_COUNT = 3
 GITHUB_SKIP_FORK = True
 GITHUB_SHOW_USER_LINK = True
@@ -65,9 +65,9 @@ GITHUB_SHOW_USER_LINK = True
 
 # Social widget
 SOCIAL = (
-    ('LinkedIn', 'https://www.linkedin.com/in/ben-chuanlong-du-1239b221/'),
-    ('Twitter', 'https://twitter.com/longendu'),
-    ('Facebook', 'https://www.facebook.com/chuanlong.du')
+    ("LinkedIn", "https://www.linkedin.com/in/ben-chuanlong-du-1239b221/"),
+    ("Twitter", "https://twitter.com/longendu"),
+    ("Facebook", "https://www.facebook.com/chuanlong.du")
 )
 
 DEFAULT_PAGINATION = 6
@@ -78,30 +78,26 @@ DEFAULT_PAGINATION = 6
 
 # This requires Pelican 3.3+
 STATIC_PATHS = [
-            'images', 
-            'figures', 
-            'downloads', 
-            'favicon.png', 
-            'media',
-            'CNAME',
-            'readme.md',
+            "images", 
+            "figures", 
+            "downloads", 
+            "favicon.png", 
+            "media",
+            "CNAME",
+            "readme.md",
         ]
 
-CODE_DIR = 'downloads/code'
-NOTEBOOK_DIR = 'downloads/notebooks'
+CODE_DIR = "downloads/code"
+NOTEBOOK_DIR = "downloads/notebooks"
 
-
-# theme and plugins
-CSS_FILE = 'main_2.css'
-THEME = os.path.join(BLOG_DIR, "themes/octopress_2")
+# theme
+CSS_FILE = "main_2.css"
+THEME = BLOG_DIR / "themes/octopress_2"
 # plugins
-IPYNB_USE_METACELL = True
-MARKUP = ('md', 'ipynb')
-#PLUGIN_PATHS = [os.path.join(BLOG_DIR, 'plugins'), BLOG_DIR / "plugins/ipynb", "/workspace/blog/plugins/ipynb/"]
-PLUGIN_PATHS = [os.path.join(BLOG_DIR, 'plugins')]
+MARKUP = ("md", "ipynb")
+PLUGIN_PATHS = [BLOG_DIR / "plugins"]
 PLUGINS = [
-        'latex',
-        #'nb_markup',
+        "latex",
     ]
 IPYNB_MARKUP_USE_FIRST_CELL = True
 IGNORE_FILES = [".ipynb_checkpoints"]  
@@ -110,8 +106,8 @@ IGNORE_FILES = [".ipynb_checkpoints"]
 DISQUS_SITENAME = "dclong"
 
 # Sharing
-TWITTER_USER = 'longendu'
-GOOGLE_PLUS_USER = 'duchuanlong'
+TWITTER_USER = "longendu"
+GOOGLE_PLUS_USER = "duchuanlong"
 GOOGLE_PLUS_ONE = True
 GOOGLE_PLUS_HIDDEN = False
 FACEBOOK_LIKE = True
@@ -119,13 +115,12 @@ TWITTER_TWEET_BUTTON = True
 TWITTER_LATEST_TWEETS = True
 TWITTER_FOLLOW_BUTTON = True
 TWITTER_TWEET_COUNT = 3
-TWITTER_SHOW_REPLIES = 'false'
-TWITTER_SHOW_FOLLOWER_COUNT = 'true'
-
+TWITTER_SHOW_REPLIES = "false"
+TWITTER_SHOW_FOLLOWER_COUNT = "true"
 
 # RSS/Atom feeds
 FEED_DOMAIN = SITEURL
-FEED_ATOM = 'atom.xml'
+FEED_ATOM = "atom.xml"
 # FEED_RSS = 
 
 
@@ -136,7 +131,7 @@ FEED_ATOM = 'atom.xml'
 SITESEARCH = "https://www.bing.com/search"
 
 # google analytics
-GOOGLE_ANALYTICS = 'UA-30259661-1'
+GOOGLE_ANALYTICS = "UA-30259661-1"
 
 # Feed generation is usually not desired when developing
 #FEED_ALL_ATOM = None
