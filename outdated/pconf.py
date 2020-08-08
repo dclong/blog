615dcb3cb95dd304f1ca35d0c1c91d2258c4220d
Ben Chuanlong Du <legendu.net@outlook.com>
2020-08-08 14:01:28 -0700

    ...

--- outdated/pconf.py
+++ outdated/pconf.py
@@ -110,7 +110,7 @@ TWITTER_SHOW_FOLLOWER_COUNT = "true"
 FEED_DOMAIN = SITEURL
 FEED_ATOM = "atom.xml"
 
-SEARCH_URL = f"{HOME_URL}/search"
+SEARCH_URL = "search"
 
 # google analytics
 GOOGLE_ANALYTICS = "UA-30259661-1"
