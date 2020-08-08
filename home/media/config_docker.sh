478b982e512cdf8af4ef36e7769cc1a5a81cc06a
Ben Chuanlong Du <legendu.net@outlook.com>
2020-08-07 18:51:50 -0700

    ...

--- home/media/config_docker.sh
+++ home/media/config_docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-ln -svf /home_host/$(id -un)/archives $HOME/
-ln -svfT /workdir/user/$(id -un) $HOME/projects
+if [ -d /home_host/$(id -un)/archives/ ]; then
+    ln -svf /home_host/$(id -un)/archives $HOME/
+fi
+if [ -f /workdir/user/$(id -un) ]; then
+    ln -svfT /workdir/user/$(id -un) $HOME/projects
+fi
 
 xinstall sshc -c
 xinstall --sudo git -ic
