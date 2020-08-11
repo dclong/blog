798028b5d5ba0b367d6c8538140b20428fc7d547
Ben Chuanlong Du <legendu.net@outlook.com>
2020-08-11 09:05:16 -0700

    ...

---
+++ misc/content/python_logging_1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import logging
+logging.basicConfig(level=20)
+logging.info("a testing message")
---
+++ misc/content/python_logging_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import logging
+logging.basicConfig(level=20)
+logging.basicConfig(level=50)
+logging.info("a testing message")
