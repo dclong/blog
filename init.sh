9bfb7ed59199be7e066c63ebc5ce8a9bba1c60d9
Benjamin Du <longendu@yahoo.com>
2020-12-12 16:50:19 -0800

    ...

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 # deps
-python3 -m pip install -r requirements.txt
+python3 -m pip install \
+    pelican \ 
+    pelican-jupyter \
+    pelican-render-math \
+    beautifulsoup4 \
+    typogrify \
+    git+https://github.com/dclong/dsutil@main
 
 # git submodules
 git submodule init && git submodule update --recursive --remote
