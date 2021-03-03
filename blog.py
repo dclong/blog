55d61e64060e0364fd318f247fb547426715a81b
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-03-03 09:16:58 -0800

    Update blog.py

--- blog.py
+++ blog.py
@@ -830,8 +830,13 @@ def _subparse_convert(subparsers):
     subparser_convert.set_defaults(func=convert)
 
 
-def parse_args(args=None, namespace=None):
-    """Parse command-line arguments for the blogging util.
+def parse_args(args=None, namespace=None) -> Namespace:
+    """Parse command-line arguments.
+    
+    :param args: The arguments to parse. 
+        If None, the arguments from command-line are parsed.
+    :param namespace: An inital Namespace object.
+    :return: A namespace object containing parsed options.
     """
     parser = ArgumentParser(description="Write blog in command line.")
     subparsers = parser.add_subparsers(dest="sub_cmd", help="Sub commands.")
