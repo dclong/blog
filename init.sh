95531ac0ccf56196e5e62da6717bfe9df59011e6
Benjamin Du <longendu@yahoo.com>
2021-04-15 18:21:25 -0700

    ...

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ python3 -m pip install \
     loguru \
     beautifulsoup4 typogrify \
     pelican pelican-jupyter pelican-render-math \
-    git+https://github.com/dclong/dsutil@main
+    "dsutil[jupyter] @ git+https://github.com/dclong/dsutil@main"
 
 # git submodules
 git submodule init && git submodule update --recursive --remote
